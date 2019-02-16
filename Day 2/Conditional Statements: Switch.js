cdc6969bf4596874ed5b0e7568e164e66996fd9d
Brian Fiszman <13773140+brianfiszman@users.noreply.github.com>
2019-02-16 13:07:37 -0300

    Added a more readable syntax

--- Day 2/Conditional Statements: Switch.js
+++ Day 2/Conditional Statements: Switch.js
@@ -1,9 +1,5 @@
 const getLetter = s =>
-	s.match(/^[aeiou]/)
-		? "A"
-		: s.match(/^[bcdfg]/)
-			? "B"
-			: s.match(/^[hjklm]/)
-				? "C"
-				: "D";
-
+    (s.match(/^[aeiou]/) && "A") ||
+    (s.match(/^[bcdfg]/) && "B") ||
+    (s.match(/^[hjklm]/) && "C") ||
+    "D";
